92ff6fdea9383aae49f83d1fe14c5c87ce667b13
Williams Isaac <williamscalg@gmail.com>
2020-04-09 15:37:07 +0100

    add global title

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Router } from 'react-router-dom';
 import history from "./services/history";
 import Routes from "./routes/index";
 import GlobalStyles from "./components/styles/global";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
       <Router history={history}>
+        <Helmet>
+            <title>ShowFlix - Premium TV Service</title>
+        </Helmet>
         <Routes />
         <GlobalStyles />
       </Router>
