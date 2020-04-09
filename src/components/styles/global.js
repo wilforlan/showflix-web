e2229be160e8b2b1e7c55feeb68efc7e223645eb
Williams Isaac <williamscalg@gmail.com>
2020-04-09 15:37:22 +0100

    remov import in global styles

--- src/components/styles/global.js
+++ src/components/styles/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap');  
     * {
         box-sizing: border-box;
     }
